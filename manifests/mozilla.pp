b1a935704c8db7975786d5a9063380b1734174f8
root <root@puppet.srce.hr>
2016-11-08 13:04:18 +0100

    new mozilla repo

---
+++ manifests/mozilla.pp
@@ -0,0 +1,20 @@
+# Class: aptrepo::mozilla
+#
+# This module manages Mozilla repo
+#
+# Sample Usage:
+#   include aptrepo::mozilla
+#
+
+class aptrepo::mozilla (
+){
+  include ::apt
+  ::apt::source { 'mozilla':
+    location          => 'http://mozilla.debian.net/',
+    release           => "${::lsbdistcodename}-backports",
+    architecture      => 'amd64',
+    repos             => 'firefox-release',
+    key               => { 'id' => '85F06FBC75E067C3F305C3C985A3D26506C4AE2A', 'server' => 'pks.aaiedu.hr', },
+    include           => { src => true },
+  }
+}
