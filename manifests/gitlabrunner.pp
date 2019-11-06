fe94d0029a189a89acdf9420774cc643585b6931
ajurjevi <26479807+ajurjevi@users.noreply.github.com>
2019-11-06 11:26:33 +0100

    Add GitLab Runner repository (#6)

---
+++ manifests/gitlabrunner.pp
@@ -0,0 +1,20 @@
+# Class: aptrepo::gitlabrunner
+#
+# This module manages Gitlab Runner repo
+#
+# Sample Usage:
+#   include aptrepo::gitlabrunner
+#
+
+class aptrepo::gitlabrunner (
+){
+  include ::apt
+  ::apt::source { 'gitlabrunner':
+    location     => 'https://packages.gitlab.com/runner/gitlab-runner/debian/',
+    release      => $::lsbdistcodename,
+    architecture => 'amd64',
+    repos        => 'main',
+    key          => { 'id' => '1A4C919DB987D435939638B914219A96E15E78F4', 'server' => 'hkp.srce.hr', },
+    include      => { src => true },
+  }
+}
