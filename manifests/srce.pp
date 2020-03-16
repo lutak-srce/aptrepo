e07e6454727eb8bbf4edc5ca6ddade24c87d2c51
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-03-16 14:27:24 +0100

    Remove srce-keyring package from Srce repo

--- manifests/srce.pp
+++ manifests/srce.pp
@@ -1,4 +1,5 @@
-# Class: aptrepo::srce
+#
+# = class: aptrepo::srce
 #
 # This module manages Srce Debian repo
 #
@@ -7,13 +8,15 @@
 #
 
 class aptrepo::srce (){
+
   include ::apt
-  ::apt::source { 'srce':
+
+  ::apt::source { 'srce' :
     location => 'http://ftp.srce.hr/srce-debian/',
     release  => "srce-${::lsbdistcodename}",
     repos    => 'main mon',
     key      => { 'id' => 'E2FFF7957AEC9D5118B95BE2FECB42104089CBA3', 'server' => 'hkp.srce.hr', },
     include  => { src => true },
   }
-  package { 'srce-keyring': ensure => present, }
+
 }
