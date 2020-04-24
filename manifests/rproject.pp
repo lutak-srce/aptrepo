a6dcb15c3290f908ef80e840a9f672eecc73dec8
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-04-24 09:30:11 +0200

    Add R Project repo

---
+++ manifests/rproject.pp
@@ -0,0 +1,19 @@
+#
+# = Class: aptrepo::rproject
+#
+# This module manages R repo
+#
+
+class aptrepo::rproject (){
+
+  include apt
+
+  apt::source { 'rproject' :
+    location => 'https://cloud.r-project.org/bin/linux/debian',
+    release  => "$::lsbdistcodename-cran35/",
+    repos    => '',
+    key      => { 'id' => 'E19F5F87128899B192B1A2C2AD5F960A256A04AF', 'server' => 'hkp.srce.hr', },
+    include  => { src => false },
+  }
+
+}
