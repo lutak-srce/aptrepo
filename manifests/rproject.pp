6344712aa4ae4c82c63c14bcb067e49e1711496d
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-04-24 09:34:23 +0200

    Add R Project repo

--- manifests/rproject.pp
+++ manifests/rproject.pp
@@ -10,7 +10,7 @@ class aptrepo::rproject (){
 
   apt::source { 'rproject' :
     location => 'https://cloud.r-project.org/bin/linux/debian',
-    release  => "$::lsbdistcodename-cran35/",
+    release  => "${::lsbdistcodename}-cran35/",
     repos    => '',
     key      => { 'id' => 'E19F5F87128899B192B1A2C2AD5F960A256A04AF', 'server' => 'hkp.srce.hr', },
     include  => { src => false },
