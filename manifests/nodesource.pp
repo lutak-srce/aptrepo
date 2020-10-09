65d5d5461554d2d9d043ab406026c55d28e129c7
Ivan Rako <irako@kosjenka.srce.hr>
2020-10-09 12:10:43 +0200

    Upgrade nodejs repo to ver 13.

--- manifests/nodesource.pp
+++ manifests/nodesource.pp
@@ -10,7 +10,7 @@
 class aptrepo::nodesource (){
   include ::apt
   ::apt::source { 'nodesource' :
-    location => 'https://deb.nodesource.com/node_12.x',
+    location => 'https://deb.nodesource.com/node_13.x',
     release  => "${::lsbdistcodename}",
     repos    => 'main',
     key      => { 'id' => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280', 'server' => 'hkp.srce.hr', },
