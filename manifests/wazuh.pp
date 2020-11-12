2e3d9d29e7a2fc75cbed1d371df181de2c67d48a
Ante Jurjevic <ante.jurjevic@srce.hr>
2020-11-12 10:38:44 +0100

    Add version variable to wazuh repo

--- manifests/wazuh.pp
+++ manifests/wazuh.pp
@@ -6,15 +6,19 @@
 # Sample Usage:
 #   include aptrepo::wazuh
 #
+class aptrepo::wazuh (
+  $version = '3',
+){
 
-class aptrepo::wazuh (){
-  include ::apt
-  ::apt::source { 'wazuh':
+  include apt
+
+  apt::source { 'wazuh':
     comment  => 'This is the WAZUH Debian repository',
-    location => 'https://packages.wazuh.com/3.x/apt/',
+    location => "https://packages.wazuh.com/${version}.x/apt/",
     release  => 'stable',
     repos    => 'main',
     key      => { 'id' => '0DCFCA5547B19D2A6099506096B3EE5F29111145', 'source' => 'https://packages.wazuh.com/key/GPG-KEY-WAZUH', },
     include  => { src => false, deb => true },
   }
+
 }
