4ff125df0488551348bcef9af8a93cdab0aa7ecc
Ante Jurjevic <ante.jurjevic@srce.hr>
2021-07-01 11:12:47 +0200

    Add haproxy repo

---
+++ manifests/haproxy.pp
@@ -0,0 +1,23 @@
+#
+# Class: aptrepo::haproxy
+#
+# This module manages haproxy repo
+#
+# Sample Usage:
+#   include aptrepo::haproxy
+#
+class aptrepo::haproxy (
+  $version = '2.4',
+){
+
+  include apt
+
+  apt::source { 'haproxy':
+    location => 'http://haproxy.debian.net',
+    release  => "buster-backports-${version}",
+    repos    => 'main',
+    key      => { 'id' => 'AEF2348766F371C689A7360095A42FE8353525F9', 'server' => 'hkp.srce.hr', },
+    include  => { src => false, deb => true },
+  }
+
+}
