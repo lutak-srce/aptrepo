5666d8e0798fd8f4de5515f0192f4cd65602978a
Ante Jurjevic <ante.jurjevic@srce.hr>
2021-08-02 11:04:31 +0200

    Change key id for puppet repo

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -13,7 +13,7 @@ class aptrepo::puppet (){
     location => 'http://apt.puppetlabs.com/',
     release  => $::lsbdistcodename,
     repos    => 'puppet6',
-    key      => { 'id' => '6F6B15509CF8E59E6E469F327F438280EF8D349F', 'server' => 'hkp.srce.hr', },
+    key      => { 'id' => 'D6811ED3ADEEB8441AF5AA8F4528B6CD9E61EF26', 'server' => 'hkp.srce.hr', },
     include  => { src => false },
   }
 }
