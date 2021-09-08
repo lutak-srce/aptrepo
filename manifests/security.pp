8258cd90777213056af11d8d3c92edbb910a9c14
Ante Jurjevic <ante.jurjevic@srce.hr>
2021-09-08 11:34:38 +0200

    Add bullseye to aptrepo::security

--- manifests/security.pp
+++ manifests/security.pp
@@ -8,12 +8,28 @@
 #
 
 class aptrepo::security (){
-  include ::apt
-  ::apt::source { 'security':
-    location => 'http://security.debian.org/',
-    release  => "${::lsbdistcodename}/updates",
-    repos    => 'main contrib non-free',
-    key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-    include  => { src => true },
+
+  include apt
+
+  case $::lsbdistcodename {
+    default: {
+        apt::source { 'security':
+          location => 'http://security.debian.org/',
+          release  => "${::lsbdistcodename}/updates",
+          repos    => 'main contrib non-free',
+          key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+          include  => { src => true },
+        }
+    }
+    /bullseye/: {
+        apt::source { 'security':
+          location => 'http://security.debian.org/',
+          release  => "${::lsbdistcodename}-security",
+          repos    => 'main contrib non-free',
+          key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+          include  => { src => true },
+        }
+    }
   }
+
 }
