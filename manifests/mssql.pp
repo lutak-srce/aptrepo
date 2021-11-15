ac03912c100788a71f81019a0f76567fe2050448
ajurjevi <26479807+ajurjevi@users.noreply.github.com>
2021-11-15 23:45:49 +0100

    Add mssql repo

---
+++ manifests/mssql.pp
@@ -0,0 +1,21 @@
+#
+# Class: aptrepo::mssql
+#
+# This module manages mssql repo
+#
+# Sample Usage:
+#   include aptrepo::mssql
+#
+class aptrepo::mssql (){
+
+  include apt
+
+  apt::source { 'mssql':
+    location => 'https://packages.microsoft.com/debian/10/prod',
+    release  => $::lsbdistcodename,
+    repos    => 'main',
+    key      => { 'id' => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF', 'server' => 'hkp.srce.hr', },
+    include  => { src => false },
+  }
+
+}
