368f89a016e72a9601b484e59764015c4e73cf38
Ante Jurjevic <ajurjevi@srce.hr>
2023-01-11 14:31:31 +0100

    Change location for nodesource

--- manifests/nodesource.pp
+++ manifests/nodesource.pp
@@ -6,11 +6,10 @@
 # Sample Usage:
 #   include aptrepo::nodesource
 #
-
 class aptrepo::nodesource (){
   include ::apt
   ::apt::source { 'nodesource' :
-    location => 'https://deb.nodesource.com/node_13.x',
+    location => 'https://deb.nodesource.com/node_16.x',
     release  => "${::lsbdistcodename}",
     repos    => 'main',
     key      => { 'id' => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280', 'server' => 'hkp.srce.hr', },
