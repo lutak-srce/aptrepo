2c3a45ec6bc53c9d5d47ae4b4a83c232e891ba07
Ante Jurjevic <ajurjevi@srce.hr>
2023-03-30 13:01:24 +0200

    asdf

--- manifests/backports.pp
+++ manifests/backports.pp
@@ -6,7 +6,6 @@
 # Sample Usage:
 #   include aptrepo::backports
 #
-
 class aptrepo::backports (){
   case $::lsbdistcodename {
     default: {
@@ -19,12 +18,27 @@ class aptrepo::backports (){
       $debian_location = 'http://ftp.hr.debian.org/debian/'
     }
   }
-  include ::apt
-  ::apt::source { 'backports':
-    location => $debian_location,
-    release  => "${::lsbdistcodename}-backports",
-    repos    => 'main contrib non-free',
-    key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-    include  => { 'src' => true },
+
+  include apt
+
+  case $::lsbdistcodename {
+    default: {
+      apt::source { 'backports':
+        location => $debian_location,
+        release  => "${::lsbdistcodename}-backports",
+        repos    => 'main contrib non-free',
+        key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+        include  => { 'src' => true },
+      }
+    }
+    /jammy/: {
+      apt::source { 'backports':
+        location => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+        release  => "${::lsbdistcodename}-backports",
+        repos    => 'main restricted universe multiverse',
+        key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
+        include  => { 'src' => false },
+      }
+    }
   }
 }
