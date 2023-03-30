8c358aee69a36dfbb47bbe42426284701307b382
Ante Jurjevic <ajurjevi@srce.hr>
2023-03-30 15:26:38 +0200

    asdf

--- manifests/backports.pp
+++ manifests/backports.pp
@@ -1,7 +1,7 @@
 #
 # Class: aptrepo::backports
 #
-# This module manages Backports repo
+# This module manages Debian Backports repo
 #
 # Sample Usage:
 #   include aptrepo::backports
@@ -21,24 +21,12 @@ class aptrepo::backports (){
 
   include apt
 
-  case $::lsbdistcodename {
-    default: {
-      apt::source { 'backports':
-        location => $debian_location,
-        release  => "${::lsbdistcodename}-backports",
-        repos    => 'main contrib non-free',
-        key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-        include  => { 'src' => true },
-      }
-    }
-    /jammy/: {
-      apt::source { 'backports':
-        location => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
-        release  => "${::lsbdistcodename}-backports",
-        repos    => 'main restricted universe multiverse',
-        key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
-        include  => { 'src' => false },
-      }
-    }
+  apt::source { 'backports':
+    location => $debian_location,
+    release  => "${::lsbdistcodename}-backports",
+    repos    => 'main contrib non-free',
+    key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+    include  => { 'src' => true },
   }
+
 }
--- manifests/security.pp
+++ manifests/security.pp
@@ -6,37 +6,27 @@
 # Sample Usage:
 #   include aptrepo::security
 #
-
 class aptrepo::security (){
 
   include apt
 
   case $::lsbdistcodename {
     default: {
-        apt::source { 'security':
-          location => 'http://security.debian.org/',
-          release  => "${::lsbdistcodename}/updates",
-          repos    => 'main contrib non-free',
-          key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-          include  => { src => true },
-        }
+      apt::source { 'security':
+        location => 'http://security.debian.org/',
+        release  => "${::lsbdistcodename}/updates",
+        repos    => 'main contrib non-free',
+        key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+        include  => { src => true },
+      }
     }
     /bullseye/: {
-        apt::source { 'security':
-          location => 'http://security.debian.org/',
-          release  => "${::lsbdistcodename}-security",
-          repos    => 'main contrib non-free',
-          key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-          include  => { src => true },
-        }
-    }
-    /jammy/: {
       apt::source { 'security':
-        location => 'http://security.ubuntu.com/ubuntu',
+        location => 'http://security.debian.org/',
         release  => "${::lsbdistcodename}-security",
-        repos    => 'main restricted',
-        key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
-        include  => { 'src' => false },
+        repos    => 'main contrib non-free',
+        key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+        include  => { src => true },
       }
     }
   }
