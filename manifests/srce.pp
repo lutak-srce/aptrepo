8857258f496ccee597cb2b384735f3937a614a7a
Ante Jurjevic <ajurjevi@srce.hr>
2023-03-30 13:31:55 +0200

    asdf

--- manifests/srce.pp
+++ manifests/srce.pp
@@ -8,12 +8,27 @@
 #
 
 class aptrepo::srce (){
-  include ::apt
-  ::apt::source { 'srce' :
-    location => 'http://ftp.srce.hr/srce-ubuntu/',
-    release  => "srce-${::lsbdistcodename}",
-    repos    => 'main mon',
-    key      => { 'id' => 'E2FFF7957AEC9D5118B95BE2FECB42104089CBA3', 'server' => 'hkp.srce.hr', },
-    include  => { src => true },
+
+  include apt
+
+  case $::lsbdistcodename {
+    default: {
+      apt::source { 'srce' :
+        location => 'http://ftp.srce.hr/srce-debian/',
+        release  => "srce-${::lsbdistcodename}",
+        repos    => 'main mon',
+        key      => { 'id' => 'E2FFF7957AEC9D5118B95BE2FECB42104089CBA3', 'server' => 'hkp.srce.hr', },
+        include  => { src => true },
+      }
+    }
+    /jammy/: {
+      apt::source { 'srce' :
+        location => 'http://ftp.srce.hr/srce-ubuntu/',
+        release  => "srce-${::lsbdistcodename}",
+        repos    => 'main mon',
+        key      => { 'id' => 'E2FFF7957AEC9D5118B95BE2FECB42104089CBA3', 'server' => 'hkp.srce.hr', },
+        include  => { src => true },
+      }
+    }
   }
 }
