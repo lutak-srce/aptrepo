e50d83a5815147fb0e32837e84e7b558dd2d4729
Ante Jurjevic <ajurjevi@srce.hr>
2023-03-30 11:39:33 +0200

    asdf

---
+++ manifests/ubuntu.pp
@@ -0,0 +1,21 @@
+#
+# Class: aptrepo::ubuntu
+#
+# This module manages ubuntu repo
+#
+# Sample Usage:
+#   include aptrepo::ubuntu
+#
+class aptrepo::ubuntu (){
+
+  include apt
+
+  apt::source { 'ubuntu':
+    location => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+    release  => $::lsbdistcodename,
+    repos    => 'main restricted',
+    key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
+    include  => { 'src' => true },
+  }
+
+}
