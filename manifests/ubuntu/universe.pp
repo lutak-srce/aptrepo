e65037c94ed187878c06ebbe20542f3048d84467
Ante Jurjevic <ajurjevi@srce.hr>
2023-03-30 15:38:51 +0200

    asdf

---
+++ manifests/ubuntu/universe.pp
@@ -0,0 +1,37 @@
+#
+# Class: aptrepo::ubuntu::updates
+#
+# This module manages Ubuntu universe repo
+#
+# Sample Usage:
+#   include aptrepo::ubuntu::universe
+#
+class aptrepo::ubuntu::universe (){
+
+  include apt
+
+  apt::source { 'universe':
+    location => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+    release  => $::lsbdistcodename,
+    repos    => 'universe',
+    key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
+    include  => { 'src' => false },
+  }
+
+  apt::source { 'universe-updates':
+    location => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+    release  => "${::lsbdistcodename}-updates",
+    repos    => 'universe',
+    key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
+    include  => { 'src' => false },
+  }
+
+  apt::source { 'universe-security':
+    location => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
+    release  => "${::lsbdistcodename}-security",
+    repos    => 'universe',
+    key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
+    include  => { 'src' => false },
+  }
+
+}
--- manifests/updates.pp
+++ manifests/updates.pp
@@ -7,26 +7,15 @@
 #   include aptrepo::updates
 #
 class aptrepo::updates (){
+
   include apt
 
-  case $::lsbdistcodename {
-    default: {
-      apt::source { 'updates':
-        location => 'http://ftp.hr.debian.org/debian/',
-        release  => "${::lsbdistcodename}-updates",
-        repos    => 'main contrib non-free',
-        key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-        include  => { src => true },
-      }
-    }
-    /jammy/: {
-      apt::source { 'updates':
-        location => 'http://nova.clouds.archive.ubuntu.com/ubuntu/',
-        release  => "${::lsbdistcodename}-updates",
-        repos    => 'main restricted',
-        key      => { 'id' => 'F6ECB3762474EDA9D21B7022871920D1991BC93C', 'server' => 'hkp.srce.hr' },
-        include  => { 'src' => false },
-      }
-    }
+  apt::source { 'updates':
+    location => 'http://ftp.hr.debian.org/debian/',
+    release  => "${::lsbdistcodename}-updates",
+    repos    => 'main contrib non-free',
+    key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+    include  => { src => true },
   }
+
 }
