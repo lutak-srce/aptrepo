9eb9a5c5e4df9019b9190d8d0feddf76d9239013
Ante Jurjevic <ajurjevi@srce.hr>
2023-05-05 14:55:09 +0200

    Add version for nodesource

--- manifests/nodesource.pp
+++ manifests/nodesource.pp
@@ -6,12 +6,14 @@
 # Sample Usage:
 #   include aptrepo::nodesource
 #
-class aptrepo::nodesource {
+class aptrepo::nodesource (
+  $version = '16',
+){
 
   include apt
 
   apt::source { 'nodesource' :
-    location => 'https://deb.nodesource.com/node_16.x',
+    location => "https://deb.nodesource.com/node_${version}.x",
     release  => "${::lsbdistcodename}",
     repos    => 'main',
     key      => { 'id' => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280', 'server' => 'hkp.srce.hr', },
