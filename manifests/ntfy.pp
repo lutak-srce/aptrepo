f104d954cb817fe4acc4432c1ac8f4dcbac1ded9
Mihael Burlic <mburlic@srce.hr>
2023-09-18 13:13:10 +0200

    Add ntfy repo for Debian/Ubuntu

---
+++ manifests/ntfy.pp
@@ -0,0 +1,21 @@
+#
+# Class: aptrepo::ntfy
+#
+# This module manages ntfy repo
+#
+# Sample Usage:
+#   include aptrepo::ntfy
+#
+class aptrepo::ntfy {
+
+  include apt
+
+  apt::source { 'ntfy':
+    location     => 'https://archive.heckel.io/apt',
+    architecture => 'amd64',
+    release      => 'debian',
+    repos        => 'main',
+    key          => { 'id' => 'CF871F1E8399DAEF470832661D5B8EDFB2476E53', 'server' => 'archive.heckel.io', },
+    include      => { src => true },
+  }
+}
