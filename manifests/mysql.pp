6938a797961edead9f4d597fc3e11aa223312845
Mihael Burlic <mburlic@srce.hr>
2024-03-04 10:11:39 +0100

    Add key to mysql repo

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -17,6 +17,7 @@ class aptrepo::mysql (
     release  => $::lsbdistcodename,
     repos    => "mysql-apt-config mysql-${version} mysql-tools",
     pin      => '600',
+    key      => { 'id' => 'BCA43417C3B485DD128EC6D4B7B3B788A8D3785C', 'server' => 'hkp.srce.hr', },
     include  => { src => false },
   }
 }
