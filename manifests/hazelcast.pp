1bdbb80d1354161ec5caf26a8cd4c5f4b14bb9bb
Mihael Burlic <mburlic@srce.hr>
2024-03-06 10:55:01 +0100

    Add hazelcast repo

---
+++ manifests/hazelcast.pp
@@ -0,0 +1,20 @@
+#
+# Class: aptrepo::hazelcast
+#
+# This module manages Hazelcast repo
+#
+# Sample Usage:
+#   include aptrepo::hazelcast
+#
+class aptrepo::hazelcast {
+
+  include apt
+
+  apt::source { 'hazelcast':
+    location     => 'https://repository.hazelcast.com/debian',
+    release      => 'stable',
+    repos        => 'main',
+    key          => { 'id' => '39A1EBFA22C0181014C4899D860675EEBAC22464', 'server' => 'hkp.srce.hr', },
+    include      => { src => true },
+  }
+}
