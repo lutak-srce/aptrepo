6809a136d8f5e4355c83f46cfbf3ce61232a61d7
Mihael Burlic <mburlic@srce.hr>
2024-03-07 09:19:45 +0100

    Fix synax error in grafana.pp

--- manifests/grafana.pp
+++ manifests/grafana.pp
@@ -11,7 +11,7 @@ class aptrepo::grafana {
   include apt
 
   apt::source { 'grafana':
-    location     => 'https://apt.grafana.com'
+    location     => 'https://apt.grafana.com',
     release      => 'stable',
     repos        => 'main',
     key          => { 'id' => 'B53AE77BADB630A683046005963FA27710458545', 'server' => 'hkp.srce.hr', },
