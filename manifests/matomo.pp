eb411fe22cfe91b4986c3ada96799dd3faabb873
Mihael Burlic <mburlic@srce.hr>
2024-06-20 13:56:37 +0200

    Change matomo release

--- manifests/matomo.pp
+++ manifests/matomo.pp
@@ -12,7 +12,7 @@ class aptrepo::matomo {
 
   apt::source { 'matomo':
     location => 'http://debian.matomo.org/',
-    release  => 'piwik',
+    release  => 'matomo',
     repos    => 'main',
     key      => { 'id' => '1FD752571FE36FF23F78F91B81E2E78B66FED89E', 'server' => 'keys.gnupg.net', },
     include  => { src => true },
