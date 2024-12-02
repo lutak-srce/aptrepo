cae4e5f93deab7872145757da7e802d420179192
Mihael Burlic <mburlic@srce.hr>
2024-12-02 09:25:42 +0100

    Add non-free-firmware repo to repo list

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -8,12 +8,21 @@
 #
 class aptrepo::debian {
 
+  case $facts['os']['distro']['codename']{
+    'bullseye': {
+      $repo_list  = 'main contrib non-free'
+    }
+    default: {
+      $repo_list  = 'main contrib non-free non-free-firmware'
+    }
+  }
+
   include apt
 
   apt::source { 'debian':
     location => 'http://ftp.hr.debian.org/debian/',
     release  => $facts['os']['distro']['codename'],
-    repos    => 'main contrib non-free',
+    repos    => $repo_list,
     key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'hkp.srce.hr' },
     include  => { 'src' => true },
   }
