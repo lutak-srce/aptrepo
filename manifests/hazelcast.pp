8a62d8b1ae555ea14a83c6793fc747136da211b9
Mihael Burlic <mburlic@srce.hr>
2024-12-20 07:06:26 +0100

    Fix minor puppet style guideline issues

--- manifests/hazelcast.pp
+++ manifests/hazelcast.pp
@@ -11,9 +11,9 @@ class aptrepo::hazelcast {
   include apt
 
   apt::source { 'hazelcast':
-    location     => 'https://repository.hazelcast.com/debian',
-    release      => 'stable',
-    repos        => 'main',
-    key          => { 'id' => '39A1EBFA22C0181014C4899D860675EEBAC22464', 'server' => 'hkp.srce.hr', },
+    location => 'https://repository.hazelcast.com/debian',
+    release  => 'stable',
+    repos    => 'main',
+    key      => { 'id' => '39A1EBFA22C0181014C4899D860675EEBAC22464', 'server' => 'hkp.srce.hr', },
   }
 }
