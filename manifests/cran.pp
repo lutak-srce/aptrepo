bcfaf51e25116465d209ec833b3886e0661fcc7a
Petra Kvocic <pgmajner@srce.hr>
2025-01-21 12:25:54 +0100

    Comment out paramater repos in cran.pp

--- manifests/cran.pp
+++ manifests/cran.pp
@@ -13,7 +13,7 @@ class aptrepo::cran {
   apt::source { 'cran' :
     location => 'http://cloud.r-project.org/bin/linux/debian',
     release  => "${facts['os']['distro']['codename']}-cran40/",
-    repos    => '',
+    # repos    => '',
     key      => { 'id' => '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7', 'server' => 'hkp.srce.hr', },
     include  => { src => true },
   }
