c04aead737532cd4a1da74ca99ffa8b5269175a7
Ivan Rako <irako@kosjenka.srce.hr>
2025-02-27 16:19:00 +0100

    Change keyring location.

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -14,7 +14,10 @@ class aptrepo::docker {
     location => 'https://download.docker.com/linux/debian/',
     release  => $facts['os']['distro']['codename'],
     repos    => 'stable',
-    key      => { 'id' => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88', 'server' => 'hkp.srce.hr', },
+    key      => {
+      'name'   => 'docker.asc',
+      'source' => 'https://download.docker.com/linux/debian/gpg',
+    },
     include  => { src => false },
   }
 }
