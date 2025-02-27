b50cacfd6caa837d5cbc62f28565ace542a50aa6
Ivan Rako <irako@kosjenka.srce.hr>
2025-02-27 15:30:40 +0100

    Add libnvidia-container repo.

--- manifests/libnvidiacontainer.pp
+++ manifests/libnvidiacontainer.pp
@@ -1,19 +1,19 @@
 #
-# Class: aptrepo::libnvidia-container
+# Class: aptrepo::nodesource
 #
-# This module manages libnvidia-container repo
+# This module manages NVIDIA Container Toolkit repo
 #
 # Sample Usage:
-#   include aptrepo::libnvidia-container
+#   include aptrepo::nvidiacontainertoolkit
 #
-class aptrepo::libnvidiacontainer {
+class aptrepo::nvidiacontainertoolkit {
 
   include apt
 
-  apt::source { 'libnvidia-container':
-    location => 'https://nvidia.github.io/libnvidia-container/stable/deb/amd64/',
-#    release  => $facts['os']['distro']['codename'],
-#    repos    => 'stable',
+  apt::source { 'nvidiacontainertoolkit' :
+    location => "https://nvidia.github.io/libnvidia-container/stable/deb/amd64/",
+    release  => '',
+    repos    => '/',
     key      => { 'id' => 'C95B321B61E88C1809C4F759DDCAE044F796ECB0', 'server' => 'hkp.srce.hr', },
     include  => { src => false },
   }
