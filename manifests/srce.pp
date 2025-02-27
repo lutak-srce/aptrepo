6d49c09fc45bd1dea1e85c0f97c02f3accdfb74b
Ivan Rako <irako@kosjenka.srce.hr>
2025-02-27 16:33:23 +0100

    Change keyring location for Srce repo.

--- manifests/srce.pp
+++ manifests/srce.pp
@@ -22,7 +22,11 @@ class aptrepo::srce {
     location => 'http://ftp.srce.hr/srce-debian/',
     release  => $release_distro,
     repos    => 'main mon',
-    key      => { 'id' => 'E2FFF7957AEC9D5118B95BE2FECB42104089CBA3', 'server' => 'hkp.srce.hr', },
+#    key      => { 'id' => 'E2FFF7957AEC9D5118B95BE2FECB42104089CBA3', 'server' => 'hkp.srce.hr', },
+    key      => {
+      'name'   => 'srce-keyring.asc',
+      'source' => 'http://ftp.srce.hr/srce-debian/srce-keyring.asc',
+    },
     include  => { src => true },
   }
 }
