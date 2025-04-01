94f10b9d259e9d8ce0a8efc32f27929abd5bae39
Mihael Burlic <mburlic@srce.hr>
2025-04-01 20:26:02 +0200

    Add puppet pubkey

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -14,7 +14,11 @@ class aptrepo::puppet {
     location => 'http://apt.puppetlabs.com/',
     release  => $facts['os']['distro']['codename'],
     repos    => 'puppet7',
-    key      => { 'id' => 'D6811ED3ADEEB8441AF5AA8F4528B6CD9E61EF26', 'server' => 'hkp.srce.hr', },
+#    key      => { 'id' => 'D6811ED3ADEEB8441AF5AA8F4528B6CD9E61EF26', 'server' => 'hkp.srce.hr', },
+    key      => {
+      'name'   => 'pubkey.gpg',
+      'source' => 'https://apt.puppet.com/pubkey.gpg',
+    },
     include  => { src => false },
   }
 }
