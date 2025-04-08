eb0220599dbfabd0c2a019fba146fb720ec4adb4
Mihael Burlic <mburlic@srce.hr>
2025-04-08 13:10:35 +0200

    Add openvox repo

---
+++ manifests/voxpopuli.pp
@@ -0,0 +1,29 @@
+#
+# Class: aptrepo::voxpopuli
+#
+# This module manages voxpopuli repo
+#
+# Sample Usage:
+#   include aptrepo::voxpopuli
+#
+class aptrepo::voxpopuli (
+  $repo = 'openvox7'
+){
+  case $facts['os']['distro']['codename'] {
+      'bullseye': { $rel  = 'debian11' }
+      'bookworm': { $rel  = 'debian12' }
+      'jammy'   : { $rel = 'ubuntu22.04'}
+    }
+  include apt
+
+  apt::source { 'voxpopuli':
+    location => 'https://apt.overlookinfratech.com/',
+    release  => $rel,
+    repos    => $repo,
+    key      => {
+      'name'   => 'voxpopuli.gpg',
+      'source' => 'https://apt.voxpupuli.org/openvox-keyring.gpg',
+    },
+    include  => { src => false },
+  }
+}
