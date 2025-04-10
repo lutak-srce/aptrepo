7c9968d4f7851599ea167838f010f1276d973a18
Mihael Burlic <mburlic@srce.hr>
2025-04-10 08:00:23 +0200

    Change adoptium key

--- manifests/adoptium.pp
+++ manifests/adoptium.pp
@@ -13,7 +13,7 @@ class aptrepo::adoptium {
     repos    => 'main',
 #    key      => { 'id' => '3B04D753C9050D9A5D343F39843C48A565F8F04B', 'server' => 'hkp.srce.hr', },
     key      => {
-      'name'   => 'adoptium.gpg',
+      'name'   => 'adoptium.asc',
       'source' => 'https://packages.adoptium.net/artifactory/api/gpg/key/public',
     },
     include  => { src => false },
