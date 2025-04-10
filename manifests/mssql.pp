58f79347ce68eed7da82129361ef1c0b36612765
Mihael Burlic <mburlic@srce.hr>
2025-04-10 07:58:21 +0200

    Change mssql key

--- manifests/mssql.pp
+++ manifests/mssql.pp
@@ -14,7 +14,11 @@ class aptrepo::mssql {
     location => "https://packages.microsoft.com/debian/${facts['os']['distro']['release']['major']}/prod",
     release  => $facts['os']['distro']['codename'],
     repos    => 'main',
-    key      => { 'id' => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF', 'server' => 'hkp.srce.hr', },
+#    key      => { 'id' => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF', 'server' => 'hkp.srce.hr', },
+    key      => {
+      'name'   => 'mssql.asc',
+      'source' => 'https://packages.microsoft.com/keys/microsoft.asc',
+    },
     include  => { src => false },
   }
 
