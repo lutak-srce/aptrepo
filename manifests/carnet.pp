0105148cea6a952654a72216377be6280b4c0be6
Mihael Burlic <mburlic@srce.hr>
2025-05-06 06:51:28 +0200

    Change carnet keyring

--- manifests/carnet.pp
+++ manifests/carnet.pp
@@ -14,7 +14,11 @@ class aptrepo::carnet {
     location => 'http://ftp.carnet.hr/carnet-debian/',
     release  => "carnet-${facts['os']['distro']['codename']}",
     repos    => 'main non-free',
-    key      => { 'id' => '0E1183A646FC9255D1B1966453FBD252EC72006A', 'server' => 'hkp.srce.hr', },
+#    key      => { 'id' => '0E1183A646FC9255D1B1966453FBD252EC72006A', 'server' => 'hkp.srce.hr', },
+    key      => {
+      'name'   => 'carnet.asc',
+      'source' => 'http://ftp.srce.hr/srce-debian/carnet-keyring.asc',
+    },
     include  => { 'src' => true },
   }
 }
