52d305ff39b5cf9ce53517bd0f1aa2f40260f315
Mihael Burlic <mburlic@srce.hr>
2025-05-06 06:34:46 +0200

    Change mysql keyring

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -17,7 +17,11 @@ class aptrepo::mysql (
     release  => $facts['os']['distro']['codename'],
     repos    => "mysql-apt-config mysql-${version} mysql-tools",
     pin      => '600',
-    key      => { 'id' => 'BCA43417C3B485DD128EC6D4B7B3B788A8D3785C', 'server' => 'hkp.srce.hr', },
+#    key      => { 'id' => 'BCA43417C3B485DD128EC6D4B7B3B788A8D3785C', 'server' => 'hkp.srce.hr', },
+    key      => {
+      'name'   => 'mysql.asc',
+      'source' => 'http://ftp.srce.hr/srce-debian/mysql-keyring.asc',
+    },
     include  => { src => false },
   }
 }
