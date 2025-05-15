bac701d06c75f6bbe1d82d5f2a0aca150648b1b9
Mihael Burlic <mburlic@srce.hr>
2025-05-15 16:45:32 +0200

    Remove path to init.pp

--- manifests/zabbix.pp
+++ manifests/zabbix.pp
@@ -11,6 +11,7 @@ class aptrepo::zabbix (
 ){
 
   include apt
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
   apt::source { 'zabbix':
     location     => "http://repo.zabbix.com/zabbix/${zabbix_version}/debian/",
