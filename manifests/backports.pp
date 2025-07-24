b1a5042edc57e97d79b88e0202a2f1c48b692e5c
Mihael Burlic <mburlic@srce.hr>
2025-07-24 16:08:33 +0200

    Remove case from backports

--- manifests/backports.pp
+++ manifests/backports.pp
@@ -7,25 +7,11 @@
 #   include aptrepo::backports
 #
 class aptrepo::backports {
-  case $facts['os']['distro']['codename'] {
-    default: {
-      $debian_location = 'http://ftp.hr.debian.org/debian/'
-    }
-    /squeeze/: {
-      $debian_location = 'http://backports.debian.org/debian-backports/'
-    }
-    /bullseye/: {
-      $debian_location = 'http://backports.debian.org/debian-backports/'
-    }
-    /wheezy/: {
-      $debian_location = 'http://ftp.hr.debian.org/debian/'
-    }
-  }
 
   include apt
 
   apt::source { 'backports':
-    location => $debian_location,
+    location => 'http://ftp.hr.debian.org/debian/',
     release  => "${facts['os']['distro']['codename']}-backports",
     repos    => 'main contrib non-free',
     key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
