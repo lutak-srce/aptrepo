c33173adaf8f9bd0ac2fb57633ea1e90e637543d
Mihael Burlic <mburlic@srce.hr>
2025-08-11 10:46:31 +0200

    Use booworm zabbix repo for trixie

--- manifests/zabbix.pp
+++ manifests/zabbix.pp
@@ -7,14 +7,21 @@
 #   include aptrepo::zabbix
 #
 class aptrepo::zabbix (
+
   $zabbix_version = '6.0'
+
 ){
+
+  case $facts['os']['distro']['codename']{
+    default: { $zabbix_release = $facts['os']['distro']['codename'] }
+    'trixie': { $zabbix_release = 'bookworm' }
+  }
 
   include apt
 
   apt::source { 'zabbix':
     location     => "http://repo.zabbix.com/zabbix/${zabbix_version}/${downcase($facts['os']['name'])}/",
-    release      => $facts['os']['distro']['codename'],
+    release      => $zabbix_release,
     architecture => 'amd64',
     repos        => 'main',
     key          => {
