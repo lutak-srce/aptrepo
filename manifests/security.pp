aea9fb3081d2a7695b41f47c484976a66de4e26e
Mihael Burlic <mburlic@srce.hr>
2025-08-11 10:04:54 +0200

    Remove key for debian repos

--- manifests/security.pp
+++ manifests/security.pp
@@ -10,25 +10,11 @@ class aptrepo::security {
 
   include apt
 
-  case $facts['os']['distro']['codename'] {
-    default: {
-        apt::source { 'security':
-          location => 'http://security.debian.org/',
-          release  => "${facts['os']['distro']['codename']}/updates",
-          repos    => 'main contrib non-free',
-          key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-          include  => { src => true },
-        }
-    }
-    /^(bullseye|bookworm)$/: {
-        apt::source { 'security':
-          location => 'http://security.debian.org/',
-          release  => "${facts['os']['distro']['codename']}-security",
-          repos    => 'main contrib non-free',
-          key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
-          include  => { src => true },
-        }
-    }
+  apt::source { 'security':
+    location => 'http://security.debian.org/',
+    release  => "${facts['os']['distro']['codename']}-security",
+    repos    => 'main contrib non-free',
+    #key      => { 'id' => '20691DFCC2C98C47952984EE00018C22381A7594', 'server' => 'pgp.mit.edu', },
+    include  => { src => true },
   }
-
 }
