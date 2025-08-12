60138bad11dad31eec829dbbfa93642036fcac23
Mihael Burlic <mburlic@srce.hr>
2025-08-12 11:45:30 +0200

    Use ubuntu noble zabbix repo for trixie

--- manifests/zabbix.pp
+++ manifests/zabbix.pp
@@ -13,14 +13,20 @@ class aptrepo::zabbix (
 ){
 
   case $facts['os']['distro']['codename']{
-    default: { $zabbix_release = $facts['os']['distro']['codename'] }
-    'trixie': { $zabbix_release = 'bookworm' }
+    default: {
+      $zabbix_release = $facts['os']['distro']['codename']
+      $zabbix_os_name = $facts['os']['name']
+      }
+    'trixie': {
+      $zabbix_release = 'noble'
+      $zabbix_os_name = 'ubuntu'
+      }
   }
 
   include apt
 
   apt::source { 'zabbix':
-    location     => "http://repo.zabbix.com/zabbix/${zabbix_version}/${downcase($facts['os']['name'])}/",
+    location     => "http://repo.zabbix.com/zabbix/${zabbix_version}/${downcase($zabbix_os_name)}/",
     release      => $zabbix_release,
     architecture => 'amd64',
     repos        => 'main',
