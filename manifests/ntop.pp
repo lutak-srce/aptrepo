261db5d780f972d470959e8e473cf1f01025d726
Ivan Rako <irako@kosjenka.srce.hr>
2026-03-18 00:03:04 +0100

    Add ntop repo.

---
+++ manifests/ntop.pp
@@ -0,0 +1,31 @@
+#
+# Class: aptrepo::ntop
+#
+# This module manages ntop repo
+#
+# Sample Usage:
+#   include aptrepo::ntop
+#
+class aptrepo::ntop {
+
+  include apt
+
+  apt::source { 'ntop-x64':
+    location => "https://packages.ntop.org/apt-stable/${facts['os']['distro']['codename']}/",
+    release  => 'x64',
+    key      => {
+      'name'   => 'ntop.asc',
+      'source' => 'https://packages.ntop.org/apt-stable/ntop.key',
+    },
+    include  => { src => false },
+  }
+  apt::source { 'ntop-all':
+    location => "https://packages.ntop.org/apt-stable/${facts['os']['distro']['codename']}/",
+    release  => 'all',
+    key      => {
+      'name'   => 'ntop.asc',
+      'source' => 'https://packages.ntop.org/apt-stable/ntop.key',
+    },
+    include  => { src => false },
+  }
+}
