09456f02d12bb37a239c12e826e1351041d02afd
Mihael Burlic <mburlic@srce.hr>
2026-04-21 14:30:48 +0200

    Add veeamagent repo

---
+++ manifests/veeamagent.pp
@@ -0,0 +1,25 @@
+#
+# Class: aptrepo::veeamagent
+#
+# This module manages veeamagent repo
+# deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/DEB-9BB5AC67.gpg] https://repository.veeam.com/backup/linux/agent-13/dpkg/debian/public stable veeam
+# Sample Usage:
+#   include aptrepo::veeamagent
+#
+class aptrepo::veeamagent {
+
+  include apt
+
+  apt::source { 'veeamagent':
+    location     => 'https://repository.veeam.com/backup/linux/agent-13/dpkg/debian/public',
+    release      => 'stable',
+    repos        => 'veeam',
+    architecture => 'amd64',
+    key          => {
+      'name'   => 'veeam.gpg',
+      'source' => 'https://repository.veeam.com/apt/public.key',
+    },
+    include      => { src => false, deb => true },
+  }
+
+}
